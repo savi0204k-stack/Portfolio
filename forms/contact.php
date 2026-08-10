302f07e7204009e5538bcf86a4dd6414e80180f2
K.Savishahan <savi0204k@gmail.com>
2026-08-10 20:18:21 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -28,7 +28,7 @@
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'savi@gmail.com',
-    'password' => '38BGe$!Fn$pj.pX',
+    'password' => 'service_pjr22ns',
     'port' => '587'
   );
   
